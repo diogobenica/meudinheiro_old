76f0038fd3744b08804eb7ac90e211ff65963e3b
Diogo Benicá <diogo.pereira@abril.com.br>
2012-12-26 16:30:37 -0200

    Iniciliza o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meudinheiro::Application.config.secret_token = 'cc1c1e0d279a5ff52c8c889396fa2532536156c9e5455db5742ac86167e84f83083de9dfb543f230f8938c856e4c83f3f88eceb0d7cb55c7cfca5edd5f6a565a'
